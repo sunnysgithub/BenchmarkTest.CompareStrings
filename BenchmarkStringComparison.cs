b82a10b994b68a58c4ebb90089ff7b9ae08e85f1
Sunny Singh <sunnysingh@posteo.de>
2022-10-03 23:25:24 +0200

    Better Benchmarks
    Adds more benchmarks and changes the position of the needle in the hay

--- BenchmarkStringComparison.cs
+++ BenchmarkStringComparison.cs
@@ -6,8 +6,9 @@ namespace BenchmarkTest.CompareStrings;
 [MemoryDiagnoser(false)]
 public class BenchmarkStringComparison
 {
-    private string hay = "avocados, bananas, mangos, melons, peaches, pineapples";
-    private string needle = "peaches";
+    private const string hayWithNeedleAtTheStart = "apples Cumin coconut sugar creamy cauliflower alfredo sauce pineapple salsa dark and stormy Italian pepperoncini mint lime taco salsa blueberry chia seed jam shaved almonds Bolivian rainbow pepper ultimate vegan chia seeds pumpkin candy cane winter sesame soba noodles balsamic vinaigrette street style Thai basil tacos basil coconut rice toasted hazelnuts lime mango crisp. Chinese five-spice powder red amazon pepper ginger tofu almond milk miso dressing red pepper smoky maple tempeh glaze cool cucumbers Thai super chili burritos cherry bomb. Indian spiced lemonade zest leek lavender lemonade bento box cocoa roasted brussel sprouts black bean wraps raspberries matcha zesty tofu pad thai lemon red lentil soup picnic salad.";
+    private const string hayWithNeedleAtTheEnd = "Cumin coconut sugar creamy cauliflower alfredo sauce pineapple salsa dark and stormy Italian pepperoncini mint lime taco salsa blueberry chia seed jam shaved almonds Bolivian rainbow pepper ultimate vegan chia seeds pumpkin candy cane winter sesame soba noodles balsamic vinaigrette street style Thai basil tacos basil coconut rice toasted hazelnuts lime mango crisp. Chinese five-spice powder red amazon pepper ginger tofu almond milk miso dressing red pepper smoky maple tempeh glaze cool cucumbers Thai super chili burritos cherry bomb. Indian spiced lemonade zest leek lavender lemonade bento box cocoa roasted brussel sprouts black bean wraps raspberries matcha zesty tofu pad thai lemon red lentil soup picnic salad apples.";
+    private string needle = "apples";
 
     private Regex regex;
     private Regex compiledRegex;
@@ -20,11 +21,20 @@ public class BenchmarkStringComparison
     }
     
     [Benchmark]
-    public bool StringContains() => hay.Contains(needle);
-
-    [Benchmark]
-    public bool RegexIsMatch() => regex.IsMatch(hay);
+    public bool StringContainsStart() => hayWithNeedleAtTheStart.Contains(needle);
     
     [Benchmark]
-    public bool CompiledRegexIsMatch() => compiledRegex.IsMatch(hay);
+    public bool StringContainsEnd() => hayWithNeedleAtTheEnd.Contains(needle);
+
+    [Benchmark]
+    public bool RegexIsMatchStart() => regex.IsMatch(hayWithNeedleAtTheStart);
+
+    [Benchmark]
+    public bool RegexIsMatchEnd() => regex.IsMatch(hayWithNeedleAtTheEnd);
+    
+    [Benchmark]
+    public bool CompiledRegexIsMatchStart() => compiledRegex.IsMatch(hayWithNeedleAtTheStart);
+    
+    [Benchmark]
+    public bool CompiledRegexIsMatchEnd() => compiledRegex.IsMatch(hayWithNeedleAtTheEnd);
 }
--- README.md
+++ README.md
@@ -12,8 +12,15 @@ Run benchmark by your own
 
 ## Results
 
-|               Method |      Mean |    Error |   StdDev | Allocated |
-|--------------------- |----------:|---------:|---------:|----------:|
-|       StringContains |  15.65 ns | 0.080 ns | 0.067 ns |         - |
-|         RegexIsMatch | 120.85 ns | 0.505 ns | 0.394 ns |         - |
-| CompiledRegexIsMatch |  58.87 ns | 0.861 ns | 0.719 ns |         - |
+|                    Method |       Mean |     Error |    StdDev | Allocated |
+|-------------------------- |-----------:|----------:|----------:|----------:|
+|       StringContainsStart |   9.545 ns | 0.0465 ns | 0.0412 ns |         - |
+|         StringContainsEnd | 667.414 ns | 2.9984 ns | 2.6580 ns |         - |
+|         RegexIsMatchStart |  90.543 ns | 0.6134 ns | 0.5438 ns |         - |
+|           RegexIsMatchEnd | 791.708 ns | 5.2943 ns | 4.9523 ns |         - |
+| CompiledRegexIsMatchStart |  44.792 ns | 0.2423 ns | 0.2148 ns |         - |
+|   CompiledRegexIsMatchEnd | 482.058 ns | 1.3081 ns | 1.2236 ns |         - |
+
+## Lesson Learned 
+
+As always in life - it depends.
